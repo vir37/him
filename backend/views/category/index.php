585c49f3b67841d7158cd937acd098093ce32dce
Alexander Pertsev <vir37@rambler.ru>
2017-01-25 21:59:17 +0300

    backend

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -1,9 +1,22 @@
 <?php
 /* @var $this yii\web\View */
-?>
-<h1>category/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+
+$this->title = 'Список категорий';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="category-index">
+    <h3><?= Html::encode($this->title) ?></h3>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            test
+        </div>
+    </div>
+
+    <p>
+        You may change the content of this page by modifying
+        the file <code><?= __FILE__; ?></code>.
+    </p>
+</div>
