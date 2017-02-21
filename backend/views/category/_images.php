f741425983ad193f958d210f6b0dc136068e1d27
Alexander Pertsev <vir37@rambler.ru>
2017-02-21 22:50:45 +0300

    backend:category

--- backend/views/category/_images.php
+++ backend/views/category/_images.php
@@ -12,22 +12,27 @@ use yii\helpers\Html;
 <?php $form = ActiveForm::begin([
     'action' => 'image-upload',
     'options' => ['enctype' => 'multipart/form-data'],
-    'layout' => 'horizontal',
+    'layout' => 'inline',
     'fieldConfig' => [
-        'horizontalCssClasses' => [
-            'label' => 'col-lg-2 col-md-3 col-sm-4',
-        ]
+        'labelOptions' => [ 'class' => 'control-label' ],
     ]])
 ?>
-    <?= $form->field($model, 'objectId', ['template' => '{input}'])->hiddenInput()->label(False) ?>
-    <?= $form->field($model, 'imageFile')->fileInput([
+    <div class="row">
+        <?= $form->field($model, 'objectId', ['template' => '{input}'])->hiddenInput()->label(False) ?>
+        <?= $form->field($model, 'imageFile', [
+            'labelOptions' => [ 'class' => 'control-label col-lg-5 col-md-5' ],
+            'options' => [ 'class' => 'form-group col-lg-6 col-md-6 col-sm-6'],
+        ])->fileInput([
             'disabled' => $linkModel->id ? false: true,
-            'class' => 'btn btn-default'
+            'class' => 'btn btn-default col-lg-6 col-md-6'
         ])->label('Файл с изображением')
-    ?>
-    <?= $form->field($model, 'isMain')->checkbox()?>
-    <?= Html::submitButton('Загрузить', [
-        'class' => 'btn btn-primary',
-        'disabled' => $linkModel->id ? false: true,
-    ])?>
+        ?>
+        <?= $form->field($model, 'isMain')->checkbox()?>
+    </div>
+    <div class="row">
+        <?= Html::submitButton('Загрузить', [
+            'class' => 'btn btn-primary',
+            'disabled' => $linkModel->id ? false: true,
+        ])?>
+    </div>
 <?php ActiveForm::end() ?>
