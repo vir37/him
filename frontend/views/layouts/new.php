2c35d83ab48f398a45bf27cb2a0fb49b56209901
Alexander Pertsev <vir37@rambler.ru>
2017-04-13 23:03:23 +0300

    Frontend: verstka

--- frontend/views/layouts/new.php
+++ frontend/views/layouts/new.php
@@ -11,11 +11,14 @@ use frontend\assets\NewAppAsset;
 use common\widgets\Alert;
 
 NewAppAsset::register($this);
+$city = \Yii::$app->params['city'];
+$phone = "8 800 888 88 88";
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
@@ -36,10 +39,11 @@ NewAppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        '<li><a href="tel:'.$phone.'"><span class="phone">'.$phone.'</span></a><span class="subline">Звонок по России бесплатный</span></li>',
+        [ 'label' => 'КОНТАКТЫ', 'url' => ['/site/contact', 'city' => $city ]],
+        [ 'label' => 'НАЙТИ', 'url' => '#' , 'options' => [ 'class' => 'search']],
     ];
+    /*
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
@@ -52,9 +56,9 @@ NewAppAsset::register($this);
             )
             . Html::endForm()
             . '</li>';
-    }
+    }*/
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav navbar-right navbar-nav-tera'],
         'items' => $menuItems,
     ]);
     NavBar::end();
