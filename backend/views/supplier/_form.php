2cbadd90e7d7dedf7812579c1b5812f948294d63
Alexander Pertsev <vir37@rambler.ru>
2017-06-19 22:50:52 +0300

    МОдуль поставщиков

--- backend/views/supplier/_form.php
+++ backend/views/supplier/_form.php
@@ -6,6 +6,8 @@ use yii\bootstrap\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model common\models\Supplier */
 /* @var $form yii\widgets\ActiveForm */
+$img = strlen($model->logo) > 10 ? 'data:image/jpeg;charset=utf-8;base64,' . base64_encode($model->logo) : '/icons/no_logo.png';
+$img = Html::img($img, [ 'style' => 'width: 150px; margin-bottom: 10px; ', 'alt' => 'NO PHOTO', ]);
 ?>
 
 <div class="supplier-form">
@@ -32,6 +34,7 @@ use yii\bootstrap\ActiveForm;
             <?= $form->field($model, 'INN', [
                 'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}',
                 'options' => [ 'class' => 'form-group col-lg-3 col-md-3'],
+                'inputOptions' => [ 'class' => 'form-control', 'id' => 'inn'],
                 'labelOptions' => [ 'class' => 'control-label col-lg-4 col-md-4'],
                 'wrapperOptions' => [ 'class' => 'col-lg-8 col-md-8'],
             ])->textInput(['maxlength' => true]) ?>
@@ -39,6 +42,7 @@ use yii\bootstrap\ActiveForm;
             <?= $form->field($model, 'OGRN', [
                 'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}',
                 'options' => [ 'class' => 'form-group col-lg-3 col-md-3'],
+                'inputOptions' => [ 'class' => 'form-control', 'id' => 'ogrn'],
                 'labelOptions' => [ 'class' => 'control-label col-lg-4 col-md-4'],
                 'wrapperOptions' => [ 'class' => 'col-lg-8 col-md-8'],
             ])->textInput(['maxlength' => true]) ?>
@@ -50,27 +54,87 @@ use yii\bootstrap\ActiveForm;
                 'wrapperOptions' => [ 'class' => 'col-lg-9 col-md-9'],
             ])->textarea(['rows' => 6])->label('Описание') ?>
 
-        <?= $form->field($model, 'jur_address_id')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'jur_address_id', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}'.
+                        Html::a('<span class="glyphicon glyphicon-pencil"></span>', '#', [
+                            'class' => 'btn btn-default',
+                        ]),
+                'options' => [ 'class' => 'form-group col-lg-12 col-md-12' ],
+                'inputOptions' => [ 'disabled' => 'disabled'],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-9 col-md-9'],
+            ])->textInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'fact_address_id')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'fact_address_id', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}'.
+                        Html::a('<span class="glyphicon glyphicon-pencil"></span>', '#', [
+                            'class' => 'btn btn-default',
+                        ]),
+                'options' => [ 'class' => 'form-group col-lg-12 col-md-12' ],
+                'inputOptions' => [ 'disabled' => 'disabled'],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-9 col-md-9'],
+            ])->textInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'post_address_id')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'post_address_id', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}'.
+                          Html::a('<span class="glyphicon glyphicon-pencil"></span>', '#', [
+                              'class' => 'btn btn-default',
+                          ]),
+                'options' => [ 'class' => 'form-group col-lg-12 col-md-12' ],
+                'inputOptions' => [ 'disabled' => 'disabled'],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-9 col-md-9'],
+            ])->textInput(['maxlength' => true]) ?>
 
-        <?= $form->field($model, 'logo')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'logo', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}'.$img,
+                'options' => [ 'class' => 'form-group col-lg-12 col-md-12' ],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-4 col-md-4'],
+            ])->fileInput([ 'accept' => 'image/jpeg,image/png']) ?>
 
-        <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'phone', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}',
+                'options' => [ 'class' => 'form-group col-lg-4 col-md-4' ],
+                'inputOptions' => [ 'class' => 'form-control', 'placeholder' => 'Телефоны' ],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-10 col-md-10'],
+            ])->textInput(['maxlength' => true])->label('<span class="glyphicon glyphicon-phone-alt"></span>') ?>
 
-        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'email', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}',
+                'options' => [ 'class' => 'form-group col-lg-4 col-md-4' ],
+                'inputOptions' => [ 'class' => 'form-control', 'placeholder' => 'Email' ],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-10 col-md-10'],
+            ])->textInput(['maxlength' => true])->label('<span class="glyphicon glyphicon-envelope"></span>') ?>
 
-        <?= $form->field($model, 'site')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'site', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}',
+                'options' => [ 'class' => 'form-group col-lg-4 col-md-4' ],
+                'inputOptions' => [ 'class' => 'form-control', 'placeholder' => 'WEB-сайт' ],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-10 col-md-10'],
+            ])->textInput(['maxlength' => true])->label('<span class="glyphicon glyphicon-globe"></span>') ?>
 
-        <?= $form->field($model, 'note')->textarea(['rows' => 6]) ?>
+            <?= $form->field($model, 'note', [
+                'template' => '{label}{beginWrapper}{input}{error}{hint}{endWrapper}',
+                'options' => [ 'class' => 'form-group col-lg-12 col-md-12'],
+                'labelOptions' => [ 'class' => 'control-label col-lg-2 col-md-2'],
+                'wrapperOptions' => [ 'class' => 'col-lg-10 col-md-10'],
+            ])->textarea(['rows' => 3]) ?>
 
-        <div class="form-group">
-            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         </div>
+        <div class="panel-footer">
+            <?= Html::submitButton( 'Сохранить', ['class' => 'btn btn-primary', 'name' => 'save']) ?>
         </div>
     </div>
     <?php ActiveForm::end(); ?>
-
 </div>
+<script type="text/javascript">
+    window.addEventListener('load', function(){
+        $('#inn').mask('9999999999?99');
+        $('#inn').mask('9999999999999');
+    });
+</script>
