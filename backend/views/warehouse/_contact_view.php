eaf22dcc12f847b890f794f2acbeb4d60d730152
Alexander Pertsev <vir37@rambler.ru>
2017-07-08 23:05:35 +0300

    Склады

--- backend/views/warehouse/_contact_view.php
+++ backend/views/warehouse/_contact_view.php
@@ -8,18 +8,21 @@
 use yii\helpers\Html;
 ?>
 <div data-key="<?= $key ?>" class="row contact-view">
-    <div class="col-lg-4 col-md-4">
+    <div class="col-lg-1 col-md-1">
         <?= Html::a('<span class="glyphicon glyphicon-remove"></span>',
             [ '/warehouse/unlink-contact', 'contact_id' => $key, 'id' => $_params_['warehouse']->id ],
             [
+                'class' => 'btn',
                 'title' => 'Отвязать контакт',
                 'data' => [
                     'confirm' => 'Вы действительно хотите отвязать контакт?',
                 ],
             ]
         ) ?>
-        <?= $model->FIO ?>
     </div>
-    <div class="col-lg-4 col-md-4"><span class="glyphicon glyphicon-phone-alt"></span><?= toLink($model->phones, 'tel') ?></div>
-    <div class="col-lg-4 col-md-4"><span class="glyphicon glyphicon-envelope"></span><?= toLink($model->emails, 'mailto') ?></div>
+    <div class="col-lg-11 col-md-11">
+        <div class="col-lg-4 col-md-4"><?= $model->FIO ?></div>
+        <div class="col-lg-4 col-md-4"><span class="glyphicon glyphicon-phone-alt">&nbsp;</span><?= toLink($model->phones, 'tel') ?></div>
+        <div class="col-lg-4 col-md-4"><span class="glyphicon glyphicon-envelope">&nbsp;</span><?= toLink($model->emails, 'mailto') ?></div>
+    </div>
 </div>
